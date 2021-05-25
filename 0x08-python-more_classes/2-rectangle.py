70cc0e9d9ccb79863002af1be57732bf7ebe994b
ilyes01 <ilyesghrab7@gmail.com>
2021-05-25 05:18:53 -0700

    rec

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""function empty rectangle
+"""
+
+
+class Rectangle:
+    """function empty rectangle
+    """
+    pass
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if (self.height == 0 or self.width == 0):
+            return 0
+        return 2 * self.height + 2 * self.width
+
+    def setWidth(self, width):
+        self.width = width
+
+    def setHeight(self, height):
+        """ height
+        args
+        self
+        value"""
+        self.__height = value
+        if not(isinstance(value, int)):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise TypeError('height must be >= 0')
+
+    def getStats(self):
+        return "area:      %s\nperimeter: %s" % (self.area(), self.perimeter())
