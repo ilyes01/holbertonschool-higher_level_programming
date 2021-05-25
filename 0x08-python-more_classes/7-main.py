ace456323f43a16b6a3b74bbf8499afe21069b44
ilyes01 <ilyesghrab7@gmail.com>
2021-05-25 05:29:30 -0700

    rec

---
+++ 0x08-python-more_classes/7-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+Rectangle = __import__('7-rectangle').Rectangle
+
+my_rectangle_1 = Rectangle(8, 4)
+print(my_rectangle_1)
+print("--")
+my_rectangle_1.print_symbol = "&"
+print(my_rectangle_1)
+print("--")
