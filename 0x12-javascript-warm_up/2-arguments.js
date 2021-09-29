21596d9a3c971f549d91a926ffbbe89470e677b8
ilyes01 <ilyesghrab7@gmail.com>
2021-09-29 14:44:46 +0100

    argument

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
