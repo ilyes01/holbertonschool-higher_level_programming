6579d760bdcc29ebcb8c4684d84e8de10e899941
ilyes01 <ilyesghrab7@gmail.com>
2021-11-04 11:11:19 +0100

    post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the body of the response of a curl POST request
+# Displays the body of the response
 curl -sX POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
