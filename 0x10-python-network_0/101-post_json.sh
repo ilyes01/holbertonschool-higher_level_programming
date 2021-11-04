0ad2e718c9141530696e5260eb3fd47242e06ac8
ilyes01 <ilyesghrab7@gmail.com>
2021-11-04 11:17:08 +0100

    post

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the body of the response of a curl POST request with json
+# Displays the body of the response
 curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
