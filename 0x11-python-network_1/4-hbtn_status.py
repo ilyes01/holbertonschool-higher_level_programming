b9ac9c71caab00eaec2e2e268c50ce918aa58d38
ilyes01 <ilyesghrab7@gmail.com>
2021-11-08 23:12:27 +0100

    statuss

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
+''' Python script that fetches https://intranet.hbtn.io/status '''
 import requests
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
+
+
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
-    print('Body response:\n\
-\t- type: {}\n\
-\t- content: {}'.format(type(text), text))#!/usr/bin/python3
-import requests
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
-if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
-    print('Body response:\n\
-\t- type: {}\n\
-\t- content: {}'.format(type(text), text))
+    req = requests.get('https://intranet.hbtn.io/status')
+    text = req.text
+    print('Body response:')
+    print('\t- type: {}'.format(type(text)))
+    print('\t- content: {}'.format(text))
